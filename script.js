af8158d9c51d085706175a9e4d22c44473015270
JonatanHellqvist <jhellkvist@live.se>
2023-10-24 11:45:27 +0200

    localstorage added

--- script.js
+++ script.js
@@ -4,22 +4,50 @@ let newTodo = document.getElementById("newTodo");
 let saveNewTodoBtn = document.getElementById("saveNewTodoBtn");
 let todoList = document.getElementById("todoList");
 
-let thingsTodo = ["Skriv ut list","Lägg till nytt", "Ta bort från listan", "Spara och gå hem"];
+
+if (localStorage.getItem("todos")) {
+    console.log("LS existerar");
+    //Anropa funktion för inloggad
+
+} else {
+    console.log("Skapar LS");
+    let thingsTodo = ["Skriv ut list","Lägg till nytt", "Ta bort från listan", "Spara och gå hem"];
+    //spara
+    localStorage.setItem("todos", JSON.stringify(thingsTodo));
+    //Anropa funktion för ej inloggad
+}
+
+
+
+
 
 function printList(){
 
     todoList.innerHTML = "";
+    //hämta
+
+    let thingsTodo = JSON.parse(localStorage.getItem("todos"));
+    console.log("List from LS", thingsTodo);
 
     for (item of thingsTodo) {
         let li = document.createElement("li");
         li.innerText = item;
         li.id = item;
 
+        //ta bort
         li.addEventListener("click",(e) => {
+
+            //hämta
+            let thingsTodo = JSON.parse(localStorage.getItem("todos"));
+            //ändra
+            thingsTodo.splice(thingsTodo.indexOf(e.target.id),1);
+            //spara
+            localStorage.setItem("todos", JSON.stringify(thingsTodo));
+
             console.log("event",e.target.id);
             console.log("click på item");
 
-            thingsTodo.splice(thingsTodo.indexOf(e.target.id),1);
+            
             console.log("efter splice", thingsTodo);
             printList();
         })
@@ -30,7 +58,15 @@ function printList(){
 
 saveNewTodoBtn.addEventListener("click", () => {
     
+    //hämta
+    let thingsTodo = JSON.parse(localStorage.getItem("todos"));
+
+    //ändra
     thingsTodo.push(newTodo.value)
+
+    //spara
+    localStorage.setItem("todos", JSON.stringify(thingsTodo));
+    
     console.log("list after click", thingsTodo);
     newTodo.value = "";
     printList();
